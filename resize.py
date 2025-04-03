31adb26d15e8d7f77032781551791e3c28c870c9
julesvanirsel <julesvanirsel@gmail.com>
2025-04-03 16:51:45 -0400

    317 lab page

--- resize.py
+++ resize.py
@@ -9,8 +9,10 @@ def main():
         width, height = read_size(img_file)
         if width > max_width:
             new_height = int(max_width * height / width)
-
-
+            new_img = resize_image(img_file, max_width, new_height)
+            new_img_file = img_file[:-4] + '-small' + img_file[-4:]
+            print(new_img_file)
+            new_img.save(new_img_file)
 
 def read_images(direc):
     files = os.listdir(direc)
@@ -21,6 +23,11 @@ def read_size(img_file):
     with Image.open(img_file) as img:
         width, height = img.size
     return width, height
+
+def resize_image(img_file, width, height):
+    with Image.open(img_file) as img:
+        new_size = (width, height)
+        return img.resize(new_size)
 
 if __name__ == '__main__':
     main()
